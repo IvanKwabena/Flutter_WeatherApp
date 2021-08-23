eba157281f4906a21c2076aba10c85670b6bec81
IvanKwabena <ikasante003@st.ug.edu.gh>
2021-08-23 22:45:55 +0000

    Initail Commit

---
+++ clima/lib/screens/loading_screen.dart
@@ -0,0 +1,38 @@
+import 'package:clima/services/location.dart';
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            //Get the current location
+            getLocation();
+          },
+          child: Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
---
+++ clima/lib/services/location.dart
@@ -0,0 +1,20 @@
+import 'dart:ffi';
+
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double longitude;
+  double latitude;
+
+  // ignore: missing_return
+  Future<Void> getCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      latitude = position.latitude;
+      longitude = position.longitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
